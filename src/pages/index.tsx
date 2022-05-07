e38ea9bd61ac9ecbc012f4f0e745e0bcf450e2d2
1mamute <gabrielbassinello@hotmail.com>
2022-05-07 17:32:42 -0300

    feat: adicionado font newake e alterado titulo da pagina

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,15 +16,13 @@ export const Home: NextPage<{ name: string; date: Date }> = ({ name, date }) => 
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{" "}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+        <h1 className="font-newake text-6xl font-bold tracking-wider">
+          <span className="underline underline-offset-2 ">UNDER</span>
+          <span>SHOWS</span>
         </h1>
 
         <p className="mt-3 text-2xl">
-          Get started by editing <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">pages/index.tsx</code>
+          Get started by editing <code className="font-mono rounded-md bg-gray-100 p-3 text-lg">pages/index.tsx</code>
         </p>
 
         <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
