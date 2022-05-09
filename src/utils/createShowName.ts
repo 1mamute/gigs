3537706937e1375b4b39ddb47903b53a975b0c9a
1mamute <gabrielbassinello@hotmail.com>
2022-05-09 10:28:32 -0300

    fix: corrigido createShowName

--- src/utils/createShowName.ts
+++ src/utils/createShowName.ts
@@ -3,12 +3,14 @@ import upperfirst from "lodash.upperfirst";
 import { format, parse } from "date-fns";
 export default function createShowName(show: Show): string {
   if (!show.name) {
-    return show.bands
+    const showName = show.bands
       .map(band => {
-        return upperfirst(band.name); // coloca a primeira letra do nome de cada banda em maiúsculo
+        return upperfirst(band.toString()); // coloca a primeira letra do nome de cada banda em maiúsculo
       })
       .join(", ") // liga as bandas com ,
-      .concat(format(parse(show.date, "dd/MM/yyyy", new Date()), "dd/MM")); // adiciona o dia e o mês do show no nome
+      .concat(" - ", format(parse(show.date, "dd/MM/yyyy", new Date()), "dd/MM")); // adiciona o dia e o mês do show no nome
+
+    return showName;
   } else {
     return show.name;
   }
