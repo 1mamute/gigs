5141c289a22feaf65cfe29588da5e87c100efa58
1mamute <gabrielbassinello@hotmail.com>
2022-05-08 22:05:35 -0300

    feat: quebra de linha no footer em telas pequenas

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,13 +5,17 @@ export const Footer: NextComponentType = () => {
   return (
     <footer className="flex justify-center items-center w-full h-24 border-t ">
       <a
-        className="flex gap-2 justify-center items-center"
+        className="flex gap-2 mx-4 text-center md:mx-0"
         href="https://github.com/undershows/gigs"
         target="_blank"
         rel="noopener noreferrer"
       >
         <p>
-          O <Logo textSize="text-l" /> é um projeto colaborativo. Colabore você também.
+          O <Logo textSize="text-l" /> é um projeto colaborativo.{" "}
+          <span className="sm:hidden">
+            <br></br>
+          </span>
+          Colabore você também.
         </p>
       </a>
     </footer>
