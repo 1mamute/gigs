970c5ddff8208884ffe9e7c142efdea12eb078b3
1mamute <gabrielbassinello@hotmail.com>
2022-05-10 12:56:35 -0300

    fix: melhoramentos do logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,18 +1,15 @@
 type LogoProps = {
   textSize?: string;
-  className?: string;
   invertColors?: boolean;
 };
 
-export default function Logo({ textSize, className, invertColors }: LogoProps) {
+export default function Logo({ textSize, invertColors }: LogoProps) {
   return (
-    <span
-      className={`font-newake font-bold tracking-wider ${textSize ? textSize : `text-6xl`} ${
-        className ? className : ""
-      }`}
-    >
-      <span className={`underline underline-offset-4 text-black ${invertColors ? `invert` : ""}`}>UNDER</span>
-      <span className={`px-1 pt-2 text-white bg-clip-padding bg-black ${invertColors ? `invert` : ""}`}>SHOWS</span>
+    <span className={`font-newake font-bold tracking-wider ${textSize ? textSize : `text-6xl`}`}>
+      <span className={`underline underline-offset-4 text-black pt-2 ${invertColors ? ` invert ` : ``}`}>UNDER</span>
+      <span className={`px-1 pt-2 text-white bg-clip-padding bg-black hover:invert ${invertColors ? ` invert ` : ``}`}>
+        SHOWS
+      </span>
     </span>
   );
 }
